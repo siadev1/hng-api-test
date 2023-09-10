cda22e74e1fcf17ffda2f3b1896c0b8ad0ac823b
siadev1 <104177113+siadev1@users.noreply.github.com>
2023-09-10 17:54:49 +0100

    Update index.php

--- hng-api-test/index.php
+++ hng-api-test/index.php
@@ -1,26 +1,11 @@
 <?php
+header("Content-Type: application/json");
 // Get the query parameters
 $slackName = isset($_GET['slack_name']) ? $_GET['slack_name'] : "";
 $track = isset($_GET['track']) ? $_GET['track'] : "";
+$url= "http://localhost/api/api.php?slack_name=$slackName&track=$track";
+$responsed=file_get_contents($url);
 
-// Get the current day and UTC time
-$Day = date("l");
-$UTCTime = gmdate("Y-m-d\TH:i:s\Z");
-    $status = 200;
+echo $responsed;
 
-
-// Construct the JSON response
-$response = array(
-    "slack_name" => $slackName,
-    "current_day" => $Day,
-    "utc_time" => $UTCTime,
-    "track" => $track,
-    "github_file_url" => "https://github.com/siadev1/hng-api-test.git",
-    "github_repo_url" => "https://github.com/username/repo",
-    "status_code" => 200
-);
-
-// Set JSON content type and send response
-header("Content-Type: application/json");
-echo json_encode($response);
-?>
+// ?>
